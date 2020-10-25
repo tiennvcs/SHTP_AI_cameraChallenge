d45a8bc6c479f3861f603f28bc3d22ef384e8c85
tiennvuit <tiennvuit@gmail.com>
2020-10-26 05:58:38 +0700

    init window gui

---
+++ config.py
@@ -0,0 +1,19 @@
+SIZE_WINDOW_GUI = {
+	'WIDTH': 800,
+	'HEIGHT': 500,
+}
+
+
+FONT = 'Arial Bold'
+
+
+TEXTS = {
+	'camera_window_title': 'Tien Nguyen UIT - KHMT',
+	'':'',
+}
+
+
+TEXT_POSITIONS = {
+	'camera_window_title': (SIZE_WINDOW_GUI['WIDTH']//2, 20),
+	
+}
