0f6c0edd1717662d117644cc468a625c01b21cb5
tiennvuit <tiennvuit@gmail.com>
2020-10-29 08:07:49 +0700

    add

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ import argparse
 import cv2
 from detect.yolo_detect import detect_image 
 from post_processing.post_processing import check_good
+import numpy as np
 
+
+def show_result(image: np.ndarray, isGood: bool):
+	
+	cv2.imshow('Predict image', image)
+	if isGood:
+		text = "OKE"
+	else:
+		text = "NOT GOOD"
+	cv2.putText(image, text, org=(100, 100),fontFace=1, fontScale=5,color=(0, 255, 0), thickness=2)
+	cv2.imshow('Result', image)
+	cv2.waitKey(0)
+	
+	return 
 
 def main(args):
 	
@@ -26,7 +40,7 @@ def main(args):
 	result, contours = check_good(roi_image, args['threshold'])
 
 	# Display result 
-	print(result)
+	show_result(image=image, isGood=result)
 
 
 
